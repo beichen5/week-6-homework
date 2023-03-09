d9ede4581c40d31cf6b8bc8b21d6cfb17dc56d56
beichen515 <maoss993@newschool.edu>
2023-03-08 22:47:36 -0500

    changeweek6assignment

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
-console.log('Remove this console log and get started!')
+const channels = {
+  channel1: {
+    title: 'Channel 1',
+    videoIds: ['py5lunOxo7w', 'n2INC1cQZSw', 'JBm1aaSFXS8']
+  },
+  channel2: {
+    title: 'Channel 2',
+    videoIds: ['IeOJm025RlE', 'voPxA5l95L4', 'etp46Aca_UM']
+  },
+  channel3: {
+    title: 'Channel 3',
+    videoIds: ['ktR7P0p6KcE', 'eKFTSSKCzWA', 'ipf7ifVSeDU']
+  }
+};
 
-const data = {
-  channels: [
-    // Add your channel data here
-    // {
-    //   title: 'UFOs',
-    //   videoIds: [
-    //     // Add your video IDs here
-    //   ]
-    // }
-  ]
+const channel1Button = document.getElementById("channel1");
+const channel2Button = document.getElementById("channel2");
+const channel3Button = document.getElementById("channel3");
+const tvElement = document.getElementById("tv");
+
+channel1Button.addEventListener("click", function() {
+  const randomIndex = Math.floor(Math.random() * channels.channel1.videoIds.length);
+  const randomVideoId = channels.channel1.videoIds[randomIndex];
+  const iframe = createIframe(randomVideoId);
+  tvElement.innerHTML = "";
+  tvElement.appendChild(iframe);
+});
+
+channel2Button.addEventListener("click", function() {
+  const randomIndex = Math.floor(Math.random() * channels.channel2.videoIds.length);
+  const randomVideoId = channels.channel2.videoIds[randomIndex];
+  const iframe = createIframe(randomVideoId);
+  tvElement.innerHTML = "";
+  tvElement.appendChild(iframe);
+});
+
+channel3Button.addEventListener("click", function() {
+  const randomIndex = Math.floor(Math.random() * channels.channel3.videoIds.length);
+  const randomVideoId = channels.channel3.videoIds[randomIndex];
+  const iframe = createIframe(randomVideoId);
+  tvElement.innerHTML = "";
+  tvElement.appendChild(iframe);
+});
+
+function createIframe(videoId) {
+  const iframe = document.createElement("iframe");
+  iframe.setAttribute("src", "https://www.youtube.com/embed/" + videoId);
+  iframe.setAttribute("allowfullscreen", "");
+  iframe.setAttribute("frameborder", "0");
+  return iframe;
 }
